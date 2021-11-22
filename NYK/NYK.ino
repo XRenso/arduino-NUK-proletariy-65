a8dcd804652267a4d4ce4bce67dcb02b3be4f44d
ProgrammistDebilov <94727301+ProgrammistDebilov@users.noreply.github.com>
2021-11-22 12:49:16 +1000

    Add files via upload

--- NYK/NYK.ino
+++ NYK/NYK.ino
@@ -1,9 +1,13 @@
+#include <NewPing.h>
 #include <TroykaIMU.h>
 #include <LiquidCrystalRus.h>
 #include <Wire.h>
 #include <TroykaDHT.h>
 #include <TroykaRTC.h>
 #define LEN_TIME 12
+#define TRIGGER_PIN  12
+#define ECHO_PIN     13
+#define MAX_DISTANCE 500
 RTC clock;
 Barometer barometer;
 char time[LEN_TIME];
@@ -13,8 +17,10 @@ int hum;
 int hour;
 int seconds;
 int minute;
+int dist;
 LiquidCrystalRus lcd(12, 10, 11, 5, 4, 3, 2);
 DHT dht(9, DHT11);
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 void setup() {
   lcd.begin(20, 4);
   dht.begin();
@@ -31,16 +37,15 @@ void setup() {
 
 void loop() {
   ints();
-  lcd.setCursor(6,0);
   times();
-  lcd.setCursor(3,1);
   temps();
-  lcd.setCursor(13,1);
   humi();
-  lcd.setCursor(1,2);
   bars();
+  dista();
+  delay(29);
 }
 void ints (){
+  dist = sonar.ping_cm();
   bar = barometer.readPressureMillimetersHg();
   temp = dht.getTemperatureC();
   hum = dht.getHumidity();
@@ -51,6 +56,7 @@ void ints (){
   dht.read();
 }
 void times () {
+  lcd.setCursor(6,0);
   lcd.print(hour);
   lcd.print(":");
   lcd.print(minute);
@@ -58,14 +64,22 @@ void times () {
   lcd.print(seconds);
 }
 void temps (){
+  lcd.setCursor(3,1);
   lcd.print(temp);
   lcd.print("\x99""C");
 }
 void humi (){
+  lcd.setCursor(13,1);
   lcd.print(hum);
   lcd.print("\x25");
 }
 void bars () {
+  lcd.setCursor(1,2);
   lcd.print(bar);
   lcd.print("мм рт. ст.");
+}
+void dista () {
+  lcd.setCursor(1,3);
+  lcd.print(dist);
+  lcd.print("см");
 }
